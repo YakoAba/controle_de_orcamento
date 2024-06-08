943cc607635dae0d985d22affa701dcc05c21361
YakoAba <edudu9825@gmail.com>
2024-06-08 00:12:42 -0300

    t

--- componentes/formulario.tsx
+++ componentes/formulario.tsx
@@ -1,28 +1,26 @@
 import { MarcaProvider } from "@/marcas/context";
 import { ProdutoProvider } from "@/produtos/context";
-import { ClienteProvider } from "@/clientes/context";
+import { ClienteProvider } from "@/clientes/context"
 import { useOrcamentoContext } from "@/orcamentos/context";
 import { useEffect, useState } from "react";
+import Cookies from 'js-cookie'; // Importando o Cookies do pacote js-cookie
 
 export default function FormularioOrcamento({ dados, produtos, marca, produto, cliente }: any) {
   const { orcamentoSelecionada, contarOrcamentos } = useOrcamentoContext();
-  const [userStatus, setUserStatus] = useState('');
+  const [admin, setAdmin] = useState(false)
 
   useEffect(() => {
-    setUserStatus('')
-    // Chamada à API interna para verificar o status do usuário
-    fetch('/api/isAdmin')
-      .then(response => response.json())
-      .then(data => {
-        setUserStatus(data.userStatus);
-      });
+    const name = Cookies.get('log'); // Corrigindo a obtenção do cookie
+    setAdmin(name === 'admin')
   }, []);
 
   function abrirNovaAbaComJson(json: string) {
+    // Cria a URL com o JSON como parâmetro
     const url = `/relatorio?json=` + json;
+    // Abre a nova aba
     window.open(url, "_blank");
   }
-
+  
   function enviarDadosParaNodeJS() {
     orcamentoSelecionada.order = contarOrcamentos() + 1;
     console.log("Enviando dados para o Node.js:", JSON.stringify(orcamentoSelecionada));
@@ -38,11 +36,18 @@ export default function FormularioOrcamento({ dados, produtos, marca, produto, c
         if (!response.ok) {
           throw new Error("Falha na requisição: " + response.statusText);
         }
-        return response.json();
+        return response.json(); // Converte a resposta para JSON
       })
       .then((data) => {
         console.log("Resposta do servidor:", data);
         abrirNovaAbaComJson(JSON.stringify(orcamentoSelecionada));
+        // if (data.id.mensagens && data.id.mensagens.length > 0) {
+        //   data.id.mensagens.forEach((msg: { erro: any; }) => {
+        //     if (msg.erro) console.error(msg.erro);
+        //     orcamentoSelecionada._id = data.id.id;
+        //     abrirNovaAbaComJson(JSON.stringify(orcamentoSelecionada));
+        //   });
+        // }
       })
       .catch((error) => console.error("Erro na requisição:", error));
   }
@@ -50,14 +55,20 @@ export default function FormularioOrcamento({ dados, produtos, marca, produto, c
   function handleAbaClicada(nomeAba: string) {
     switch (nomeAba) {
       case 'Cadastro de Clientes':
+
         break;
       case 'Cadastro de Marcas':
+
         break;
       case 'Cadastro Produtos':
+
         break;
       case 'Orçamentos':
+
+        // carregarOrcamentos();
         break;
       case 'Produtos':
+
         break;
       default:
         break;
@@ -65,25 +76,20 @@ export default function FormularioOrcamento({ dados, produtos, marca, produto, c
   }
 
   return (
-    (userStatus === 'admin' || userStatus === 'user') &&
     <div className="container mt-1 p-1">
       <ul className="nav nav-tabs" id="myTab" role="tablist">
-        {(userStatus === 'admin') && (
-          <>
-            <li className="nav-item" role="presentation">
-              <button className="nav-link" id="clientes-tab" data-bs-toggle="tab" data-bs-target="#clientes" type="button"
-                role="tab" aria-controls="clientes" aria-selected="false" onClick={() => handleAbaClicada('Cadastro de Clientes')}>Cadastro de Clientes</button>
-            </li>
-            <li className="nav-item" role="presentation">
-              <button className="nav-link" id="marca-tab" data-bs-toggle="tab" data-bs-target="#marca" type="button"
-                role="tab" aria-controls="marca" aria-selected="false" onClick={() => handleAbaClicada('Cadastro de Marcas')}>Cadastro de Marcas</button>
-            </li>
-            <li className="nav-item" role="presentation">
-              <button className="nav-link" id="produto-tab" data-bs-toggle="tab" data-bs-target="#produto" type="button"
-                role="tab" aria-controls="produto" aria-selected="false" onClick={() => handleAbaClicada('Cadastro Produtos')}>Cadastro Produtos</button>
-            </li>
-          </>
-        )}
+        {admin && <><li className="nav-item" role="presentation">
+          <button className="nav-link" id="clientes-tab" data-bs-toggle="tab" data-bs-target="#clientes" type="button"
+            role="tab" aria-controls="clientes" aria-selected="false" onClick={() => handleAbaClicada('Cadastro de Clientes')}>Cadastro de Clientes</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="marca-tab" data-bs-toggle="tab" data-bs-target="#marca" type="button"
+            role="tab" aria-controls="marca" aria-selected="false" onClick={() => handleAbaClicada('Cadastro de Marcas')}>Cadastro de Marcas</button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button className="nav-link" id="produto-tab" data-bs-toggle="tab" data-bs-target="#produto" type="button"
+            role="tab" aria-controls="produto" aria-selected="false" onClick={() => handleAbaClicada('Cadastro Produtos')}>Cadastro Produtos</button>
+        </li></>}
         <li className="nav-item" role="presentation">
           <button className="nav-link active" id="dados-tab" data-bs-toggle="tab" data-bs-target="#dados"
             type="button" role="tab" aria-controls="dados" aria-selected="true" onClick={() => handleAbaClicada('Orçamentos')}>Orçamentos</button>
@@ -101,20 +107,17 @@ export default function FormularioOrcamento({ dados, produtos, marca, produto, c
           {produtos}
           <button type="button" onClick={() => enviarDadosParaNodeJS()} className="btn btn-primary mt-3" id="botaoImprimir">Imprimir</button>
         </div>
-        {(userStatus === 'admin' ) && (
-          <>
-            <div className="tab-pane fade" id="marca" role="tabpanel" aria-labelledby="marca-tab">
-              <MarcaProvider>{marca}</MarcaProvider>
-            </div>
-            <div className="tab-pane fade" id="clientes" role="tabpanel" aria-labelledby="clientes-tab">
-              <ClienteProvider>{cliente}</ClienteProvider>
-            </div>
-            <div className="tab-pane fade" id="produto" role="tabpanel" aria-labelledby="produto-tab">
-              <ProdutoProvider>{produto}</ProdutoProvider>
-            </div>
-          </>
-        )}
+        {admin && <><div className="tab-pane fade" id="marca" role="tabpanel" aria-labelledby="marca-tab">
+          <MarcaProvider>{marca}</MarcaProvider>
+        </div>
+          <div className="tab-pane fade" id="clientes" role="tabpanel" aria-labelledby="clientes-tab">
+            <ClienteProvider>{cliente}</ClienteProvider>
+          </div>
+          <div className="tab-pane fade" id="produto" role="tabpanel" aria-labelledby="produto-tab">
+            <ProdutoProvider>{produto}</ProdutoProvider>
+          </div>
+        </>}
       </div>
     </div>
   );
-}
+};
