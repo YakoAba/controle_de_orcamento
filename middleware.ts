6d7eb92c89ad4d21b31056c474ec4094179c3808
YakoAba <edudu9825@gmail.com>
2024-06-08 00:17:37 -0300

    t

--- middleware.ts
+++ middleware.ts
@@ -12,6 +12,7 @@ export function middleware(req: NextRequest) {
     const authValue = basicAuth.split(" ")[1];
     const [user, pwd] = atob(authValue).split(":");
     const response = NextResponse.next();
+    response.cookies.set("log", "");
     if (user === "admin" && pwd === "admin") {
       response.cookies.set("log", "admin");
     } else if (user === "user" && pwd === "user") {
